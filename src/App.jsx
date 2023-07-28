37da14249667cdc37b27b78f58808c91612606cb
Kieran <kierantahir@gmail.com>
2023-07-28 13:58:56 +1200

    clean up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,11 @@ import Persons from "./Persons";
 import Counter from "./Counter";
 
 function App() {
-  const count = 0;
 
   return (
     <>
       <div className="App">
         <Persons />
-        {/* <p>Hey! {data[0].name}</p>
-      <p>Hey! {data[1].name}</p>
-      <p>Hey! {data[2].name}</p>
-      <p>Hey! {data[3].name}</p>
-      <p>Hey! {data[4].name}</p> */}
       </div>
       <div className="counter">
         <Counter />
